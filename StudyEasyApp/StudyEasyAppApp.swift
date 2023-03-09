e9f2a4a5be2f95dbc81b12be4d18adebf0015db0
YimKeul <leesjun29@gmail.com>
2023-03-10 02:17:32 +0900

    Initial Commit

---
+++ StudyEasyApp/StudyEasyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyEasyAppApp.swift
+//  StudyEasyApp
+//
+//  Created by yimkeul on 2023/03/10.
+//
+
+import SwiftUI
+
+@main
+struct StudyEasyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
