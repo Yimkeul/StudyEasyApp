8eedf9194490f4906d67a576fe74cdbd8abb8a77
YimKeul <leesjun29@gmail.com>
2023-03-10 02:57:46 +0900

    feat : add TextField, Slider to animation, color Picker

--- StudyEasyApp/ContentView.swift
+++ StudyEasyApp/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var colors : [Color] = [.black, .red, .green, .blue]
+    var colornames : [String] = ["black", "red", "green", "blue"]
+    
+    @State private var colorIndex = 0
+    @State private var rotation : Double = 0.0;
+    @State private var text : String = "Welcome to SwfitUI";
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .rotationEffect(Angle(degrees: rotation))
+                .animation(.easeOut(duration: 5) , value: rotation)
+                .foregroundColor(colors[colorIndex])
+            Slider(value: $rotation,in: 0 ... 360 , step : 0.1)
+            Text("\(rotation)")
+            
+            TextField("Enter text here", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Picker(selection: $colorIndex , label: Text("Color")){
+                ForEach(0 ..< colornames.count , id : \.self){ index in                     Text(colornames[index])
+                        .foregroundColor(colors[index])
+                }
+            }
+
         }
-        .padding()
     }
 }
 
